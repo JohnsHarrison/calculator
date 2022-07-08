511ad3df75d0754aa0bc9e6b12ee684a22c5c4bf
JohnsHarrison <jshar15089@gmail.com>
2022-07-08 06:54:41 -0400

    addition working

---
+++ src/Components/Calculator.jsx
@@ -0,0 +1,57 @@
+import {useState} from 'react'
+function Calculator(){
+const [value1, setValue1] = useState(0)
+const [value2, setValue2] = useState(0)
+let result;
+// const [result,setResult] = useState(0)
+// let newNum;
+// function add(num1,num2){
+//  newNum=(parseInt(num1) + parseInt(num2))
+    
+//     return newNum
+// }
+// console.log(add(value1,value2))
+if(isNaN(value1)){
+    alert('please enter a number')
+    setValue1(0)
+} 
+
+if(isNaN(value2)){
+    alert('please enter a number')
+    setValue2(0)
+}
+console.log(value1)
+console.log(value2)
+console.log(result)
+    return(
+        <div className="container">
+
+  <h1>Add with React!</h1>
+
+ 
+
+  <form className="add">
+
+    <input type="text" name="value1"  onChange ={e => setValue1(e.target.value)} />
+
+    <span>+</span>
+
+    <input type="text" name="value2" onChange = {e => setValue2(e.target.value)} />
+
+    {/* <span><button onClick= {()=>(add(value1,value2))}> = </button> </span> */}
+    {/* <span><button onClick= {()=>(setResult((parseInt(value1)+(parseInt(value2)))))}> = </button> </span> */}
+    <span><button onClick= {()=>(localStorage.setItem(result,(parseInt(value1)+(parseInt(value2)))))}> = </button> </span>
+    
+
+    <h3>{localStorage.getItem(result)}</h3>
+    
+
+    <h3>Addition results go here!</h3>
+
+  </form>
+
+</div>
+    )
+}
+
+export default Calculator
